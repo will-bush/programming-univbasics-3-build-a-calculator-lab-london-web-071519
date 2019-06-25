4dc0aadca45ae9dee061d1681e8558245f442c6b
Will Bush <williamdcbush@gmail.com>
2019-06-25 22:58:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ p num1 + num2
 end
 
 def subtraction(num1, num2)
-
+p num1 + num2
 end
 
 def division(num1, num2)
-
+p num1 + num2
 end
 
 def multiplication(num1, num2)
-
+p num1 + num2
 end
 
 def modulo(num1, num2)
-
+p num1 + num2
 end
 
 def square_root(num)
-
+p num1 + num2
 end
